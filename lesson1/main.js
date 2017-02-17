8e1a23b088cbcee38314a276fac0d2ff43241a7f
elenatafeenko <elenatafeenko@gmail.com>
2017-02-17 19:51:29 +0300

    part 2,3 of lesson 2

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,6 +1,5 @@
 function ChessBoard(container) {
   var LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-  var cells = [];
   var activeCell = null;
   var chessBoard = this; // mainObject
 
@@ -26,6 +25,13 @@ function ChessBoard(container) {
     activeCell.classList.add('active');
   }
 
+  function addFigure(element, figureType, color) {
+    element.classList.remove('white', 'black', 'pown', 'rook', 'knight', 'bishop', 'queen', 'king');
+    element.classList.add(figureType);
+    element.classList.add(color);
+    element.classList.add('figure');
+  }
+
   function addCell(i, j) {
     var element = document.createElement('div');
     element.className = 'cell';
@@ -42,20 +48,21 @@ function ChessBoard(container) {
       element.style.clear = 'left';
       element.innerText = i;
     } else {
-      if (i === 1 || i === 2 || i === 7 || i === 8) { // figure
-        element.classList.add('figure');
-        if (i === 2 || i === 7) {
-          element.style.backgroundPositionX = '-100px';
-        } else if (j === 1 || j === 8) {
-          element.style.backgroundPositionX = '-80px';
-        } else if (j === 2 || j === 7) {
-          element.style.backgroundPositionX = '-60px';
-        } else if (j === 3 || j === 6) {
-          element.style.backgroundPositionX = '-40px';
-        } else if ((j === 4 && i === 1) || (j === 5 && i === 8)) {
-          element.style.backgroundPositionX = '-20px';
-        }
-      }
+      // if (i === 1 || i === 2 || i === 7 || i === 8) { // figure
+      //   if (i === 2 || i === 7) {
+      //     addFigure(element, 'pown', i < 4 ? 'black' : 'white');
+      //   } else if (j === 1 || j === 8) {
+      //     addFigure(element, 'rook', i < 4 ? 'black' : 'white');
+      //   } else if (j === 2 || j === 7) {
+      //     addFigure(element, 'knight', i < 4 ? 'black' : 'white');
+      //   } else if (j === 3 || j === 6) {
+      //     addFigure(element, 'bishop', i < 4 ? 'black' : 'white');
+      //   } else if ((j === 4 && i === 1) || (j === 5 && i === 8)) {
+      //     addFigure(element, 'queen', i < 4 ? 'black' : 'white');
+      //   } else if ((j === 5 && i === 1) || (j === 4 && i === 8)) {
+      //     addFigure(element, 'king', i < 4 ? 'black' : 'white');
+      //   }
+      // }
 
       element.classList.add('clickable');
       element.id = '' + i + LETTERS[j-1];
@@ -64,7 +71,6 @@ function ChessBoard(container) {
       });
     }
 
-    cells.push(element);
     cellsContainer.appendChild(element);
   }
 
@@ -73,6 +79,25 @@ function ChessBoard(container) {
       addCell(i, j);
     }
   }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'figures.json', true);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+    if (xhr.status !== 200) {
+      alert(xhr.status + ': ' + xhr.statusText);
+    } else {
+      var figuresData = JSON.parse(xhr.responseText);
+      for (var i = 0; i < figuresData.length; i++) {
+        var element = document.getElementById(figuresData[i].place);
+        addFigure(element, figuresData[i].type, figuresData[i].color);
+      }
+    }
+
+  };
 
   document.addEventListener('keydown', function(event) {
     if (!activeCell) {
